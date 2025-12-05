f11b881eb14405afe581ee369e41de377c02856f
sinem-codes <unutulmazunut0@gmail.com>
2025-12-05 12:13:21 +0300

    Add files via upload

---
+++ karar1.php
@@ -0,0 +1,13 @@
+﻿<?php
+$durum=30;
+if($durum>-30 && $durum<=-10)
+	echo"Çok soğuk";
+else if($durum>-10 && $durum<=0)
+	echo"Soğuk";
+else if($durum>0 && $durum<=10)
+	echo"Serin";
+else if($durum>10 && $durum<=30)
+	echo"Sıcak";
+else
+	echo"Çok Sıcak";
+?>
---
+++ mat1.php
@@ -0,0 +1,14 @@
+﻿<?php
+$a=5;
+$b=3;
+$c=9;
+$d=9.7;
+$e=77.4;
+echo "üs=".pow($a,$b)."<br>";
+echo"kaakök=".sqrt($c)."<br>";
+echo"yuvarla=".round($d)."<br>";
+echo"aşağı yuvarla=".floor($e)."<br>";
+echo"yukarı yuvarla=".ceil($e)."<br>";
+echo"rasgele sayı=".rand(800,100);
+
+?>
